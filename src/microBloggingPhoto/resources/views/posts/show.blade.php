ea2f4e104fd5bb78d004d3a988cc9d5fffb8be65
jhilaa <julien.hilaire@imelmi.net>
2021-12-02 12:48:11 +0100

    fonctions Posts ok. 1er pas tailwind css

--- src/microBloggingPhoto/resources/views/posts/show.blade.php
+++ src/microBloggingPhoto/resources/views/posts/show.blade.php
@@ -4,6 +4,7 @@
 =======
   {{$post->user}}:
 >>>>>>> 6e15f180a21dda5faf29de7da3aa13e6a62ebbe3
+  {{$post->user->name}}:
   {{$post->description}}
   <img src="{{ $post->img_url }}">
 </x-guest-layout>
